9fd0b3ea986bb3c91d5af9517bd5cd220bff3ffc
stephenpangga <42969061+stephenpangga@users.noreply.github.com>
2020-02-17 16:59:31 +0100

    finish assigment00

--- Program00/Program.cs
+++ Program00/Program.cs
@@ -17,26 +17,34 @@ namespace Program00
         {
             int value = ReadInt("Enter a value: ");
             Console.WriteLine("You entered {0}.", value);
-            /*
+            
             int age = ReadInt("How old are you? ", 0, 120);
             Console.WriteLine("You are {0} years old.", age);
-
+            
             string name = ReadString("What is your name? ");
-
             Console.WriteLine("Nice meeting you {0}.", name);
-            */
+            
             Console.ReadKey();
         }
         int ReadInt(string question)
         {
-            Console.WriteLine(question);
+            Console.Write(question);
             return int.Parse(Console.ReadLine());
         }
         int ReadInt(string question, int min, int max)
         {
             int age = 0;
-
+            do
+            {
+                age = ReadInt(question);
+            }
+            while (age < min || age > max);
             return age;
+        }
+        string ReadString(string question)
+        {
+            Console.Write(question);
+            return Console.ReadLine();
         }
     }
 }
